68416d3096e66b7e2ceb4e78ed49d4d55a040d4d
Denis <ortin@i.ua>
2017-03-27 00:13:26 +0300

    change

--- src/main/java/com/nitro/Controller/UserController.java
+++ src/main/java/com/nitro/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.nitro.Controller;
 import com.nitro.Entity.User;
 import com.nitro.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/nitro/Security/SecurityWebInitialization.java
+++ src/main/java/com/nitro/Security/SecurityWebInitialization.java
@@ -1,22 +1,37 @@
 package com.nitro.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityWebInitialization extends WebSecurityConfigurerAdapter {
 
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll();
+        http.csrf().disable();
+    }
+
+    @Autowired
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .inMemoryAuthentication().withUser("user").password("password").roles("User")
-                .and().withUser("admin").password("password").roles("USER", "ADMIN");
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER").and()
+                .withUser("admin").password("password").roles("USER", "ADMIN");
+
 
 
 
     }
-
-
 }
